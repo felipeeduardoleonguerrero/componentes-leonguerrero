8f518585e18badd054ffc72614835e3c8442a382
felipeeduardoleonguerrero <felipeeduardoleonguerrero@gmail.com>
2022-05-06 00:20:08 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MenuComponent,
+    ContentComponent
+    
   ],
   imports: [
     BrowserModule
